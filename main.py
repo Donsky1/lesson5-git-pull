b6e1814c0f1ba64e2870d4b42e3ec7bef8ce457e
Donsky1 <falson2007@yandex.ru>
2022-03-22 07:05:23 +0300

    was added 9-10-11-12 menu

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 import os
 import shutil
 import platform
+from games.victory import victory
+from games.account import accaunt
 
 CREATOR = 'Pavel & Co'
+OLD_DIR = os.getcwd()
 
 while True:
     print('\nМеню (Консольный файловый менеджер): ')
@@ -19,7 +22,8 @@ while True:
     print('9. Играть в викторину')
     print('10. Мой банковский счет')
     print('11. Смена рабочей директории')
-    print('12. Выход')
+    print('12. Выбрать исходную директорию')
+    print('13. Выход')
     print('-'*30)
 
     choice = input('Выберите пункт меню: ')
@@ -89,12 +93,28 @@ while True:
     if choice == '8':
         print('Разработчик программного продукта "Консольный файловый менеджер": ', CREATOR)
         input('Для продолжения нажмите любую клавишу ...')
+    # Играть в викторину'
     if choice == '9':
-        pass
+        print('Вы выбрали меню играть в викторину')
+        print('#'*30)
+        victory()
+    # Мой банковский счет
     if choice == '10':
-        pass
+        print('Вы выбрали меню "Мой банковский счет"')
+        print('$'*30)
+        accaunt()
+    # Смена рабочей директории
     if choice == '11':
-        pass
+        print('Введите директорию в нотиации своей ОС на которую хотите перейти: ')
+        path_dir = input('Директория: ')
+        os.chdir(path_dir)
+        print(f'Текущая директория: {os.getcwd()}')
+        input('Для продолжения нажмите любую клавишу ...')
     # выход из программы
     if choice == '12':
+        print('Возврат к исходной директории ... ')
+        os.chdir(OLD_DIR)
+        print(f'Текущая директория: {os.getcwd()}')
+        input('Для продолжения нажмите любую клавишу ...')
+    if choice == '13':
         exit()

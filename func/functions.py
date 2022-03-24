bf030d1a0e399a89d3fceed184bd74071e0e87de
Donsky1 <falson2007@yandex.ru>
2022-03-24 17:27:53 +0300

    Добавлено 2 декоратора, для отображения процесса вызова функции Некоторые функции переписаны через вложенный список, Добавлены try.. except исключения для отлавливания некоторых ошибок, которые были выявлены в процессе тестирования программы Исправлена ошибка, некорректного создания папки в корневом катале windows, возникающая когда пользователь вводит символ "/" и "\" Внесены доп.мелкие исправления

--- func/functions.py
+++ func/functions.py
@@ -1,13 +1,65 @@
 import os
 import shutil
+from tqdm import tqdm
+from time import sleep
+
+def get_info_about_function(function):
+    def inner(*args, **kwargs):
+        for i in tqdm(range(30), function.__doc__):
+            sleep(.01)
+        result = function(*args, **kwargs)
+        return result
+    return inner
+
+
+def get_info_about_function_lite(function):
+    def inner(*args, **kwargs):
+        print(function.__doc__)
+        result = function(*args, **kwargs)
+        return result
+    return inner
+
+# функция вывода меню в консоли
+def get_menu():
+    print('\nМеню (Консольный файловый менеджер): ')
+    print('1. Создать папку')
+    print('2. Удалить (файл/папку)')
+    print('3. Копировать (файл/папку)')
+    print('4. Просмотр содержимого рабочей директории')
+    print('5. Посмотреть только папки')
+    print('6. Посмотреть только файлы')
+    print('7. Просмотр информации об операционной системе')
+    print('8. Создатель программы')
+    print('9. Играть в викторину')
+    print('10. Мой банковский счет')
+    print('11. Смена рабочей директории')
+    print('12. Выбрать исходную директорию')
+    print('13. Сохранить содержимое рабочей директории в файл')
+    print('14. Выход')
+    print('-' * 30)
+
+
+# функция проверияет наличие символов, т.к если они есть то на windows нарушается логика работы программы
+def check_name_folder_or_file(name):
+    return True if ('/' in name) or ('\\' in name) else False
+
 
 # функция создания папки
+@get_info_about_function
 def create_folder(folder_name):
-    if not os.path.exists(os.path.join(os.getcwd(), folder_name)):
-        os.mkdir(folder_name)
-        return f'Создана папка {os.path.join(os.getcwd(), folder_name)}'
-    else:
-        return f'Папка с именем {folder_name} уже существует'
+    """процедура создания папки ... """
+    try:
+        if check_name_folder_or_file(folder_name):
+            raise OSError('Указаны неверные символы в названии папки')
+        else:
+            if not os.path.exists(folder_name):
+                os.mkdir(folder_name)
+                return f'Создана папка {os.path.join(os.getcwd(), folder_name)}'
+            else:
+                return f'Папка с именем {folder_name} уже существует'
+    except OSError as er:
+        return f'Вы ввели неправильное имя папки, папка с именем {folder_name} не создана\n{er}'
+
 
 # Функция ожидания ввода пользователем для продолжения выполнения программы
 def wait_input():
@@ -15,47 +67,76 @@ def wait_input():
 
 
 # функция удаления папки или файла
+@get_info_about_function
 def delete_file_folder(rm_folder_or_file):
-    if os.path.exists(rm_folder_or_file):
-        if '.' in rm_folder_or_file:
-            os.remove(rm_folder_or_file)
-            return f'Файл с именем {rm_folder_or_file} был удален'
-        else:
-            os.rmdir(rm_folder_or_file)
-            return f'Папка с именем {rm_folder_or_file} была удалена'
+    """процедура удаления папки или файла ..."""
+    if check_name_folder_or_file(rm_folder_or_file):
+        return 'Указаны неверные символы в названии папки'
     else:
-        return f'Указанной Вами файла/папки не существует'
+        if os.path.exists(rm_folder_or_file):
+            try:
+                if os.path.isfile(rm_folder_or_file):
+                    os.remove(rm_folder_or_file)
+                    return f'Файл с именем {rm_folder_or_file} был удален'
+                else:
+                    os.rmdir(rm_folder_or_file)
+                    return f'Папка с именем {rm_folder_or_file} была удалена'
+            except OSError as er:
+                return f'Удалить {rm_folder_or_file} не удалось, ошибка \n{er}'
+        else:
+            return f'Указанного файла/папки не существует'
 
 
 # функция копироваия файла или папки
+@get_info_about_function_lite
 def copy_file_folder():
+    """процедура копирования папки или файла ..."""
+    result = 'Указаны неверные символы в названии'
     folder_or_file_old = input('Введите старое название папки или файла: ')
+    if check_name_folder_or_file(folder_or_file_old):
+        return result
     if os.path.exists(folder_or_file_old):
-        if '.' in folder_or_file_old:
-            folder_or_file_new = input('Введите новое название файла: ')
-            shutil.copy(folder_or_file_old, folder_or_file_new)
-            print(f'Была создана копия файла {folder_or_file_old} на {folder_or_file_new}')
-        else:
-            folder_or_file_new = input('Введите новое название папки: ')
-            shutil.copytree(folder_or_file_old, folder_or_file_new)
-            print(f'Была создана копия папки {folder_or_file_old} на {folder_or_file_new}')
+        try:
+            if os.path.isfile(folder_or_file_old):
+                folder_or_file_new = input('Введите новое название файла: ')
+                if check_name_folder_or_file(folder_or_file_new):
+                    return result
+                shutil.copy(folder_or_file_old, folder_or_file_new)
+                return f'Была создана копия файла {folder_or_file_old} на {folder_or_file_new}'
+            else:
+                folder_or_file_new = input('Введите новое название папки: ')
+                if check_name_folder_or_file(folder_or_file_new):
+                    return result
+                shutil.copytree(folder_or_file_old, folder_or_file_new)
+                return f'Была создана копия папки {folder_or_file_old} на {folder_or_file_new}'
+        except OSError as er:
+            return f'Указаны неверные символы в названии, \n{er}'
     else:
-        print(f'Указанной Вами файла/папки не существует')
+        return result
+
 
 # функция отображения только папок
+@get_info_about_function_lite
 def look_only_folder():
+    """процедура отображения только папок ..."""
     paths = [path for path in list(os.walk(os.getcwd()))[:1]]
     _, folders, _ = paths[0]
     return folders
 
+
 # функция отображения только файлов
+@get_info_about_function_lite
 def look_only_files():
+    """процедура отображения только файлов ..."""
     paths = [path for path in list(os.walk(os.getcwd()))[:1]]
     _, _, files = paths[0]
     return files
 
+
 # функция сохр содержимого рабочей директории в файл listdir.txt
+@get_info_about_function
 def create_list_dir():
+    """процедура сохр. содержимого рабочей директории в файл listdir.txt ..."""
     paths = [path for path in list(os.walk(os.getcwd()))[:1]]
     _, folders, files = paths[0]
     folders_str = 'dir: ' + ', '.join(folders) + '\n'
--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# бесконечный цикл
-# выход по нажатию кнопки
-import sys
 import platform
 import games.account as acc
 from games.victory import victory
@@ -17,22 +14,7 @@ while True:
     total_cash = open_session_balance(BALANCE)
     history = open_session_history(HISTORY)
 
-    print('\nМеню (Консольный файловый менеджер): ')
-    print('1. Создать папку')
-    print('2. Удалить (файл/папку)')
-    print('3. Копировать (файл/папку)')
-    print('4. Просмотр содержимого рабочей директории')
-    print('5. Посмотреть только папки')
-    print('6. Посмотреть только файлы')
-    print('7. Просмотр информации об операционной системе')
-    print('8. Создатель программы')
-    print('9. Играть в викторину')
-    print('10. Мой банковский счет')
-    print('11. Смена рабочей директории')
-    print('12. Выбрать исходную директорию')
-    print('13. Сохранить содержимое рабочей директории в файл')
-    print('14. Выход')
-    print('-'*30)
+    get_menu()  # вывод меню
 
     choice = input('Выберите пункт меню: ')
     # Создать папку
@@ -41,47 +23,57 @@ while True:
         folder_name = input('Введине название папки: ')
         print(create_folder(folder_name))
         wait_input()
+
     # Удалить (файл/папку)
     elif choice == '2':
         rm_folder_or_file = input('Введите название папки или файла: ')
         print(delete_file_folder(rm_folder_or_file))
         wait_input()
+
     # Копировать (файл/папку)
     elif choice == '3':
-        copy_file_folder()
+        print(copy_file_folder())
         wait_input()
+
     # Просмотр содержимого рабочей директории
     elif choice == '4':
         print(f'Содержимое дериктории {os.getcwd()}:')
         print(os.listdir(os.getcwd()))
         wait_input()
+
     # Посмотреть только папки
     elif choice == '5':
         print(f'Содержимое папок дериктории {os.getcwd()}:')
         print(look_only_folder())
         wait_input()
+
     # Посмотреть только файлы
     elif choice == '6':
         print(f'Содержимое файлов дериктории {os.getcwd()}:')
         print(look_only_files())
         wait_input()
+
     # Просмотр информации об операционной системе
     elif choice == '7':
         print('Информации об операционной системе:')
         for i in platform.uname():
             print(i)
         wait_input()
+
     elif choice == '8':
         print('Разработчик программного продукта "Консольный файловый менеджер": ', CREATOR)
         wait_input()
+
     # Играть в викторину'
     elif choice == '9':
         print('Вы выбрали меню играть в викторину')
-        print('#'*30)
+        print('#' * 30)
         victory()
+
     # Мой банковский счет
     elif choice == '10':
         account(total_cash, history)
+
     # Смена рабочей директории
     elif choice == '11':
         print('Введите директорию в нотиации своей ОС на которую хотите перейти: ')
@@ -89,14 +81,17 @@ while True:
         os.chdir(path_dir)
         print(f'Текущая директория: {os.getcwd()}')
         wait_input()
+
     # выход из программы
     elif choice == '12':
         print('Возврат к исходной директории ... ')
         os.chdir(OLD_DIR)
         print(f'Текущая директория: {os.getcwd()}')
         wait_input()
+
     elif choice == '13':
         print(create_list_dir()[0])
         wait_input()
+
     elif choice == '14':
         exit()
